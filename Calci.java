dfab37d0b8c68e06363a5ba23ea43c6abb8d0f03
Akhila tk <atk14302@gmail.com>
2024-05-20 16:32:06 +0530

    Calci class is added

---
+++ Calci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner
+public interface Calci{
+int add(int a,int b);
+}
+class CalciImpl implements Calci{
+public int add(int a,int b){
+return a+b;
+}
+}
+class CalciDriver{
+public static void main(String[] args)
+{
+System.out.println("enter two numbers");
+Scanner sc=new Scanner(System.in);
+int n1=sc.nextInt();
+int n2=sc.nextInt();
+Calci c=new CalciImpl();
+System.out.println(c.add(n1,n2));
+sc.close();
+}
+}
